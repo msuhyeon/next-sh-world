7c846dba8236f00fc7618608011d54e46afaaf05
msuhyeon <job.maengsh@gmail.com>
2025-02-01 17:19:37 +0900

    feat: reset.css 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import MainHeader from "@/components/MainHeader";
+import "./reset.css";
 import "./globals.css";
 import styles from "@/layout.module.css";
 import MainFooter from "./components/MainFooter";
